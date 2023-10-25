e119c7c82216789c092bc48cfb1389a78a43de17
Kelvin Domingos KD <kelvindomingos20@hotmail.com>
2023-10-25 19:57:50 +0100

    estilizando login admin

--- admin/index.php
+++ admin/index.php
@@ -80,28 +80,15 @@
                         <div class="login-form">
                             <form  method="post">
                                 <div class="form-group">
-                                    <label>Email Address</label>
+                                    <label>Email</label>
                                     <input class="au-input au-input--full" type="email" name="email_telefone" placeholder="Email">
                                 </div>
                                 <div class="form-group">
-                                    <label>Password</label>
+                                    <label>Senha</label>
                                     <input class="au-input au-input--full" type="password" name="senha" placeholder="Password">
                                 </div>
-                                <div class="login-checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember">Remember Me
-                                    </label>
-                                    <label>
-                                        <a href="#">Forgotten Password?</a>
-                                    </label>
-                                </div>
+                                
                                 <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit" name="btnentrar">Login</button>
-                                <div class="social-login-content">
-                                    <div class="social-button">
-                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button>
-                                        <button class="au-btn au-btn--block au-btn--blue2">sign in with twitter</button>
-                                    </div>
-                                </div>
                             </form>
                             <div class="register-link">
                                 <p>
