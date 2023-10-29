3233af9af7ff2f46e58e4e99b6f98f3d546862c4
Kelvin Domingos KD <kelvindomingos20@hotmail.com>
2023-10-29 09:58:50 +0100

    corrigir bug upload de imagem

--- admin/postmidia.php
+++ admin/postmidia.php
@@ -16,7 +16,7 @@
          else{
              $file2 = $file["name"];
              $query = "insert into imagem (titulo,imagem,idamin) values ('$titulo','$file2','$id_admin')";
-             if(mysqli_query($connection, $query)){
+             if(mysqli_query($conexao, $query)){
                  echo "<script>alert('Mídia adicionada com sucesso!');</script>";
              }
              else{
